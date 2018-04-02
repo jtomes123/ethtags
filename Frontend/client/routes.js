cead0b04aa1e15cad42276e511b96e17013360ec
Jakub Tomeš (jtomes123) <jtomes123@gmail.com>
2018-04-02 10:30:46 +0200

    added routes to error template

--- Frontend/client/routes.js
+++ Frontend/client/routes.js
@@ -3,72 +3,78 @@ FlowRouter.route('/preview/:_id', {
     BlazeLayout.render('App_Body', {main: 'preview', address: FlowRouter.getParam('_id')});
   }
 });
+FlowRouter.route('/error/:_code', {
+  name: 'error',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'error'});
+  }
+});
 FlowRouter.route('/iframe/:_id', {
-    name: 'iframe',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id')});
-    }
-  });
-  FlowRouter.route('/iframe/:_id/:_showqr', {
-    name: 'iframe',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
-        hideqr: FlowRouter.getParam('_showqr')});
-    }
-  });
-  FlowRouter.route('/iframe/:_id/:_showqr/:_bgc', {
-    name: 'iframe',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
-        hideqr: FlowRouter.getParam('_showqr')});
-    }
-  });
-  FlowRouter.route('/iframe/:_id/:_showqr/:_bgc/:_textc', {
-    name: 'iframe',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
-        hideqr: FlowRouter.getParam('_showqr')});
-    }
-  });
-  FlowRouter.route('/', {
-    name: 'home',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'home'});
-    }
-  });
-  FlowRouter.route('/dogtag', {
-    name: 'main',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'main'});
-    }
-  });
-  FlowRouter.route('/about', {
-    name: 'about',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'about'});
-    }
-  });
-  FlowRouter.route('/faq', {
-    name: 'faq',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'faq'});
-    }
-  });
-  FlowRouter.route('/ver', {
-    name: 'verification',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'verification'});
-    }
-  });
-  FlowRouter.route('/vercon/:_id', {
-    name: 'verification-confirm',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'verificationConfirm', id: FlowRouter.getParam("_id")});
-    }
-  });
-  FlowRouter.route('/withdraw', {
-    name: 'withdraw',
-    action() {
-      BlazeLayout.render('App_Body', {main: 'withdraw'});
-    }
-  });
+  name: 'iframe',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id')});
+  }
+});
+FlowRouter.route('/iframe/:_id/:_showqr', {
+  name: 'iframe',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
+      hideqr: FlowRouter.getParam('_showqr')});
+  }
+});
+FlowRouter.route('/iframe/:_id/:_showqr/:_bgc', {
+  name: 'iframe',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
+      hideqr: FlowRouter.getParam('_showqr')});
+  }
+});
+FlowRouter.route('/iframe/:_id/:_showqr/:_bgc/:_textc', {
+  name: 'iframe',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'iframe', address: FlowRouter.getParam('_id'), 
+      hideqr: FlowRouter.getParam('_showqr')});
+  }
+});
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'home'});
+  }
+});
+FlowRouter.route('/dogtag', {
+  name: 'main',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'main'});
+  }
+});
+FlowRouter.route('/about', {
+  name: 'about',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'about'});
+  }
+});
+FlowRouter.route('/faq', {
+  name: 'faq',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'faq'});
+  }
+});
+FlowRouter.route('/ver', {
+  name: 'verification',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'verification'});
+  }
+});
+FlowRouter.route('/vercon/:_id', {
+  name: 'verification-confirm',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'verificationConfirm', id: FlowRouter.getParam("_id")});
+  }
+});
+FlowRouter.route('/withdraw', {
+  name: 'withdraw',
+  action() {
+    BlazeLayout.render('App_Body', {main: 'withdraw'});
+  }
+});
